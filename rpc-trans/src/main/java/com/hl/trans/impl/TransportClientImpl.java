9b81243500f0576115713025071a9706e46b359a
diablohl <hldiablo@hotmail.com>
2022-03-20 14:54:35 +0800

    传输模块实现server

--- rpc-trans/src/main/java/com/hl/trans/impl/TransportClientImpl.java
+++ rpc-trans/src/main/java/com/hl/trans/impl/TransportClientImpl.java
@@ -2,10 +2,15 @@ package com.hl.trans.impl;
 
 import com.hl.rpc.Peer;
 import com.hl.trans.TransportClient;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TransportClientImpl implements TransportClient {
+    private String url;
     /**
      * 连接端口
      *
@@ -13,12 +18,32 @@ public class TransportClientImpl implements TransportClient {
      */
     @Override
     public void connect(Peer peer) {
-
+        //短链接
+        this.url = "http://" + peer.getHost() + ":" + peer.getPort();
     }
 
     @Override
     public InputStream write(InputStream inputStream) {
-        return null;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(true);
+            connection.setRequestMethod("POST");
+
+            connection.connect();
+            //连接完成后要把数据发送给server
+            IOUtils.copy(inputStream, connection.getOutputStream());
+            int code = connection.getResponseCode();
+            if (code == HttpURLConnection.HTTP_OK) {
+                return connection.getInputStream();
+            } else {
+                return connection.getErrorStream();
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
